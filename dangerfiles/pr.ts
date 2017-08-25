b1ae5fdb7469bae42fbc21e9a890b65a8f55ee5e
Felipe Sabino <felipe@sabino.me>
2017-08-25 19:38:34 -0300

    Testing diff

--- dangerfiles/pr.ts
+++ dangerfiles/pr.ts
@@ -1,3 +1,5 @@
-import {message} from 'danger';
+import { danger, fail, markdown, message, schedule, warn } from 'danger'
 
 message('Hello from Peril on your PR.');
+
+message(`Diff for ${danger.git.modified_files[0]}: ${danger.git.diffForFile(danger.git.modified_files[0])}`);
