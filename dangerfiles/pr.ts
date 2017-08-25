b098eff5db8e2016fd058bad32e48503009ada5f
Felipe Sabino <felipe@sabino.me>
2017-08-25 19:44:39 -0300

    Testing diff

--- dangerfiles/pr.ts
+++ dangerfiles/pr.ts
@@ -1,5 +1,18 @@
-import { danger, fail, markdown, message, schedule, warn } from 'danger'
+import { TextDiff, danger, fail, markdown, message, schedule, warn } from 'danger'
 
 message('Hello from Peril on your PR.');
 
-message(`Diff for ${danger.git.modified_files[0]}: ${danger.git.diffForFile(danger.git.modified_files[0])}`);
+if (danger.git.modified_files.length === 0) {
+  message('There were no modified files');
+} else {
+
+  let file = danger.git.modified_files[0];
+  schedule(async () => {
+    let diff = (await danger.git.diffForFile(file) as TextDiff);
+    if (diff) {
+      message(`Diff for ${file}: ${diff.diff}`);
+    } else {
+      message(`No diff for ${file}`);
+    }
+  });
+}
